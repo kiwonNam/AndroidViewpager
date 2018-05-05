9a21d490216a0df6b6cb39c2f577e89871c16ab2
Namkiwon <originman@bluehack.net>
2018-05-06 00:40:50 +0900

    fix a text

--- app/src/main/java/namkiwon/example/com/android_viewpager/Fragment/SecondFragment.java
+++ app/src/main/java/namkiwon/example/com/android_viewpager/Fragment/SecondFragment.java
@@ -22,7 +22,7 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.fragment_first,null);
+        View view = inflater.inflate(R.layout.fragment_second,null);
 
 
 
